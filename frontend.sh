f13a46be9afc1fb8f67037b0bc5d57da9ff00047
Nagarajuchinni <nagarajchinni3@gmail.com>
2024-04-07 17:50:58 +0530

    Modified Readme file

--- frontend.sh
+++ frontend.sh
@@ -1,15 +1,17 @@
-echo -e "\e[36m installing nginx \e[0m"
-dnf install nginx -y &>>/tmp/expense.log
-echo -e "\e[35m copying expence config file \e[0m"
-cd /etc/nginx/default.d/expense.conf &>>/tmp/expense.log
-echo -e "\e[34m removing older files \e[0m"
-rm -rf /usr/share/nginx/html/* &>>/tmp/expense.log
-echo -e "\e[33m downloading frontend zip file \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/expense.log
-echo -e "\e[33m changing directory \e[0m"
-cd /usr/share/nginx/html &>>/tmp/expense.log
-echo -e "\e[33m unziping the file \e[0m"
-unzip /tmp/frontend.zip &>>/tmp/expense.log
-echo -e "\e[33m starting nginx server \e[0m"
-systemctl enable nginx &>>/tmp/expense.log
-systemctl start nginx &>>/tmp/expense.log
+log_file=/tmp/expense.log
+color=\e[35m
+echo -e "${color} installing nginx \e[0m"
+dnf install nginx -y &>>$log_file
+echo -e "${color} copying expence config file \e[0m"
+cd /etc/nginx/default.d/expense.conf &>>$log_file
+echo -e "${color} removing older files \e[0m"
+rm -rf /usr/share/nginx/html/* &>>$log_file
+echo -e "${color} downloading frontend zip file \e[0m"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+echo -e "${color}changing directory \e[0m"
+cd /usr/share/nginx/html &>>$log_file
+echo -e "${color} unziping the file \e[0m"
+unzip /tmp/frontend.zip &>>$log_file
+echo -e "${color} starting nginx server \e[0m"
+systemctl enable nginx &>>$log_file
+systemctl start nginx &>>$log_file
