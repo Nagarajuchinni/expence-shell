73d15624b8b97215b4e5dd1706b3689eef83c9f7
Nagarajuchinni <nagarajchinni3@gmail.com>
2024-04-07 21:16:53 +0530

    Modified Readme file

--- frontend.sh
+++ frontend.sh
@@ -9,14 +9,44 @@ else
 fi
 echo -e "${color} copying expence config file \e[0m"
 cd /etc/nginx/default.d/expense.conf &>>$log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Success \e[0m"
+else
+  echo -e "\e[31m FAIL \e[0m"
+fi
 echo -e "${color} removing older files \e[0m"
 rm -rf /usr/share/nginx/html/* &>>$log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Success \e[0m"
+else
+  echo -e "\e[31m FAIL \e[0m"
+fi
 echo -e "${color} downloading frontend zip file \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Success \e[0m"
+else
+  echo -e "\e[31m FAIL \e[0m"
+fi
 echo -e "${color}changing directory \e[0m"
 cd /usr/share/nginx/html &>>$log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Success \e[0m"
+else
+  echo -e "\e[31m FAIL \e[0m"
+fi
 echo -e "${color} unziping the file \e[0m"
 unzip /tmp/frontend.zip &>>$log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Success \e[0m"
+else
+  echo -e "\e[31m FAIL \e[0m"
+fi
 echo -e "${color} starting nginx server \e[0m"
 systemctl enable nginx &>>$log_file
 systemctl start nginx &>>$log_file
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m Success \e[0m"
+else
+  echo -e "\e[31m FAIL \e[0m"
+fi
